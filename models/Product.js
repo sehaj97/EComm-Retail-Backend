e3c46976c5a0a99d55aafe981c112a442cb091ae
sehaj97 <sehajmagan@gmail.com>
2022-01-21 11:40:50 -0500

    added models update and added refrences to the models

--- models/Product.js
+++ models/Product.js
@@ -7,22 +7,37 @@ class Product extends Model {}
 
 Product.init(
     {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
         product_name: {
             type: DataTypes.STRING,
             allowNull: false
         },
         price: {
             type: DataTypes.DECIMAL(10,2),
-            allowNull: false
+            allowNull: false,
+            validate: {
+                isDecimal: true
+            }
         },
         stock: {
             type: DataTypes.INTEGER,
-            allowNull: false
+            allowNull: false,
+            defaultValue: 10,
+            validate: {
+                isNumeric: true
+            }
         },
         category_id: {
             type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true
+            references: {
+                model: "category",
+                key: "id"
+            }
         }
     },
     {
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,16 +6,27 @@ class ProductTag extends Model {}
 
 ProductTag.init(
     {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
         product_id: {
             
             type: DataTypes.INTEGER,
-            
-            allowNull: false
+            references: {
+                model: "product",
+                key: "id"
+            }
         },
         
         tag_id: {
             type: DataTypes.INTEGER,
-            allowNull: false
+            references: {
+                model: "tag",
+                key: "id"
+            }
         }
     },
     {
